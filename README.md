3802fa5a09bf15b8d1a07ca172a58b34b4b9766f
snemes <snemes@users.noreply.github.com>
2020-03-30 14:38:58 +0200

    Speed improvements, merged Python 2 and 3 version into a single file.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # aplib
-A pure Python module for decompressing aPLib compressed data.
+A pure Python module for decompressing aPLib compressed data
 
-Adapted from the original C source code from http://ibsensoftware.com/files/aPLib-1.1.1.zip
-
-Approximately ~20 times faster than the other Python implementations.
+Adapted from the original C source code from http://ibsensoftware.com/files/aPLib-1.1.1.zip  
+Approximately 20 times faster than other Python implementations.  
+Compatible with both Python 2 and 3.  
--- aplib.py
+++ aplib.py
@@ -1,48 +1,36 @@
 #!/usr/bin/env python3
-"""
-A pure Python module for decompressing aPLib compressed data.
+"""A pure Python module for decompressing aPLib compressed data
+
 Adapted from the original C source code from http://ibsensoftware.com/files/aPLib-1.1.1.zip
-Approximately 20 times faster than the other Python implementations.
+Approximately 20 times faster than other Python implementations.
+Compatible with both Python 2 and 3.
 """
 import struct
 from binascii import crc32
 from io import BytesIO
 
 __all__ = ['APLib', 'decompress']
-__version__ = '0.5'
+__version__ = '0.6'
 __author__ = 'Sandor Nemes'
 
 
-class APLib:
-    """internal data structure"""
+class APLib(object):
+
+    __slots__ = 'source', 'destination', 'tag', 'bitcount', 'strict'
 
     def __init__(self, source, strict=True):
         self.source = BytesIO(source)
-        self.destination = BytesIO()
+        self.destination = bytearray()
         self.tag = 0
         self.bitcount = 0
         self.strict = bool(strict)
-
-    def read(self):
-        return ord(self.source.read(1))
-
-    def write(self, value):
-        self.destination.write(bytes([value]))
-
-    def copy(self, length=1):
-        for _ in range(length):
-            self.write(self.read())
-
-    def dstcopy(self, offset, length=1):
-        for _ in range(length):
-            self.write(self.destination.getbuffer()[-offset])
 
     def getbit(self):
         # check if tag is empty
         self.bitcount -= 1
         if self.bitcount < 0:
             # load next tag
-            self.tag = self.read()
+            self.tag = ord(self.source.read(1))
             self.bitcount = 7
 
         # shift bit out of tag
@@ -70,7 +58,7 @@ class APLib:
         try:
 
             # first byte verbatim
-            self.copy()
+            self.destination += self.source.read(1)
 
             # main decompression loop
             while not done:
@@ -82,18 +70,19 @@ class APLib:
                                 offs = (offs << 1) + self.getbit()
 
                             if offs:
-                                self.dstcopy(offs)
+                                self.destination.append(self.destination[-offs])
                             else:
-                                self.write(0)
+                                self.destination.append(0)
 
                             lwm = 0
                         else:
-                            offs = self.read()
+                            offs = ord(self.source.read(1))
                             length = 2 + (offs & 1)
                             offs >>= 1
 
                             if offs:
-                                self.dstcopy(offs, length)
+                                for _ in range(length):
+                                    self.destination.append(self.destination[-offs])
                             else:
                                 done = True
 
@@ -105,7 +94,9 @@ class APLib:
                         if lwm == 0 and offs == 2:
                             offs = r0
                             length = self.getgamma()
-                            self.dstcopy(offs, length)
+
+                            for _ in range(length):
+                                self.destination.append(self.destination[-offs])
                         else:
                             if lwm == 0:
                                 offs -= 3
@@ -113,7 +104,7 @@ class APLib:
                                 offs -= 2
 
                             offs <<= 8
-                            offs += self.read()
+                            offs += ord(self.source.read(1))
                             length = self.getgamma()
 
                             if offs >= 32000:
@@ -123,19 +114,21 @@ class APLib:
                             if offs < 128:
                                 length += 2
 
-                            self.dstcopy(offs, length)
+                            for _ in range(length):
+                                self.destination.append(self.destination[-offs])
+
                             r0 = offs
 
                         lwm = 1
                 else:
-                    self.copy()
+                    self.destination += self.source.read(1)
                     lwm = 0
 
         except (TypeError, IndexError):
             if self.strict:
-                raise RuntimeError('aPLib decompression error') from None
+                raise RuntimeError('aPLib decompression error')
 
-        return self.destination.getvalue()
+        return bytes(self.destination)
 
     def pack(self):
         raise NotImplementedError
